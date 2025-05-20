7558076fd3ae0dcbab4da55161fb2bb2b592785c
shakurShirajul <siirajulisllam@gmail.com>
2025-05-20 08:42:45 +0600

    Wishlist Button Updated

--- src/Pages/MyWishlists/MyWishlist.jsx
+++ src/Pages/MyWishlists/MyWishlist.jsx
@@ -70,7 +70,7 @@ const MyWishlist = ({ item, handleWishlistDelete }) => {
             <Link to={`/blogdetails/${_id}`} className="w-full">
               <button
                 type="button"
-                class="focus:outline-none text-white bg-[#20DC49] hover:bg-green-800 w-full  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#20DC49] dark:hover:bg-green-700 "
+                class="focus:outline-none text-white bg-green-600 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 w-full"
               >
                 View Details
               </button>
